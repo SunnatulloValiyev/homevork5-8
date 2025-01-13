558c8f9b36ff7610f4a3ab1f6caee13c36756d8c
SunnatulloValiyev <sunnatillovaliyev769@gmail.com>
2025-01-13 06:35:18 +0500

    O'zgartirishlar haqida izoh

--- main.js
+++ main.js
@@ -96,13 +96,128 @@ left && left.addEventListener('click', function () {
 
 
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     fetch("https://jsonplaceholder.typicode.com/posts")
-//         .then(function (response) {
-//             console.log(response);
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//         });
-// });
+document.addEventListener('DOMContentLoaded', function () {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "GET"
+    })
+        .then(function (response) {
+            if (response.status == 200) {
+                return response.json()
+            }
+        })
+        .then(function (data) {
+            data.forEach(function (post) {
+                console.log('Title:', post.title);
+                console.log('Body:', post.body);                
+            });            
+            console.log('1-topshiriq tugadi');
+        })
+        .catch(function (error) {
+            console.log('Xatolik yuz berdi' ,error);
+        });
+        console.log('1. Mashq: Barcha postlarni olish');
+});
 
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch("https://randomuser.me/api/", {
+        method: "GET"
+    })
+    .then(function (response) {
+        if (response.status == 200) {
+            return response.json()
+        }
+    })
+    .then(function (data) {
+        const randomIndex = Math.floor(Math.random() * data.results.length); 
+        const randomPost = data.results[randomIndex]; 
+        
+
+        console.log("Name:", randomPost.name.first); 
+        console.log("Email:", randomPost.email);
+        console.log('2-topshiriq tugadi: Random user malumotlarini olish');
+        
+    })
+
+    .catch(function (error) {
+        console.log('Xatolik yuz berdi', error);
+        
+    })    
+})
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch("https://disease.sh/v3/covid-19/all", {
+        method: "GET"
+    })
+
+    .then(function (response) {
+        if (response.status == 200) {
+            return response.json()
+        }
+    })
+    .then(function (posts) {
+        console.log("Cases:", posts.cases);
+        console.log("Deaths:", posts.deaths);
+        console.log("Recovered:", posts.recovered);
+        console.log("3-topshiriq tugati: Mashq: Dunyo bo'ylab COVID-19 statistikasi");
+    })
+    .catch(function (error) {
+        console.log("Xatolik yuz berdi", error);
+        
+    })  
+})
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('https://api.coindesk.com/v1/bpi/currentprice.json', {
+        method: "GET"
+    })
+
+    .then(function (response) {
+        if (response.status == 200) {
+            return response.json()
+        }
+    })
+    .then(function (price){
+        if (price && price.bpi && price.bpi.USD && price.bpi.USD.rate) {
+            console.log("Narx:", price.bpi.USD.rate);
+        } else {
+            console.log("Narx ma'lumoti topilmadi");
+        }
+        console.log("4-topshiriq tugadi:Mashq: Bitcoin narxi ");
+        
+    })
+    
+    .catch(function (error) {
+        console.log("Xatolik yuzaga keldi", error);
+        
+    })
+})
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let city = "Taskent";
+    fetch("https://goweather.herokuapp.com/weather/{city}", {
+        method: "GET"
+    })
+
+    .then(function (response) {
+        if (response.status == 200) {
+            return response.json()
+        }
+    })
+    .then(function (temp){
+        console.log(`${city}, shahar harorati ${temp.temperature}`);
+    })
+
+    .catch(function (error){
+        console.log('Xatolik yuzaga keldi', error);
+        
+    })
+
+})
